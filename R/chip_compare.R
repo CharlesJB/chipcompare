31e1bebf2a3751adb3ea95a931df2f17b01396cd
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2014-08-19 14:17:53 -0400

    New version of chip_compare that produce matrix when instanciated
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- R/chip_compare.R
+++ R/chip_compare.R
@@ -2,12 +2,20 @@
 #' 
 chip_compare <- R6::R6Class("chip_compare",
   public = list(
+    ## initialize
     initialize = function(grlist1, grlist2=NULL) {
       stopifnot(class(grlist1) == "GRangesList")
       stopifnot(is.null(grlist2) | class(grlist2) == "GRangesList")
       private$grl[[1]] <- grlist1
       private$grl[[2]] <- grlist2
+      private$score_matrix <- private$produce_matrix()
     },
+    ## set_compute_score_FUN
+    set_compute_score_FUN = function(FUN) {
+      stopifnot(class(FUN(private$grl[[1]], private$grl[[1]])) == "numeric")
+      private$compute_score <- FUN
+    },
+    ## print
     print = function(...) {
       message <- paste0("chip_compare Object of length: ",
                         length(private$grl), "\n")
@@ -27,6 +35,32 @@ chip_compare <- R6::R6Class("chip_compare",
     }
   ),
   private = list(
-    grl = list()
+    #### private members
+    grl = list(),
+    score_matrix = matrix(),
+
+    #### private methods
+    ## compute_score
+    compute_score = function(qry, sbj) {
+      count <- length(unique(queryHits(findOverlaps(qry, sbj))))
+      count / length(qry)
+    },
+    ## produce_matrix
+    produce_matrix = function() {
+      query <- private$grl[[1]]
+      if (length(private$grl) == 2) {
+        subject <- private$grl[[2]]
+      } else {
+        subject <- private$grl[[1]]
+      }
+      q_length <- length(query)
+      s_length <- length(subject)
+      result <- matrix(nrow = q_length, ncol = s_length)
+      for (i in 1:q_length) {
+        for (j in 1:s_length) {
+          result[i,j] <- private$compute_score(query[i], subject[j])
+        }
+      }
+    }
   )
 )

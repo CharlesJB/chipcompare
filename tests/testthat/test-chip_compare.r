127c890d96404490a78c88a13a399b2e783eda66
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2014-08-19 16:23:42 -0400

    Cleaned tests
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- tests/testthat/test-chip_compare.r
+++ tests/testthat/test-chip_compare.r
@@ -1,3 +1,5 @@
+### PREPARE TESTS
+
 # Prepare GRanges objects
 bed1 <- data.frame(seqnames="chr2", start=3, end=6, strand="+")
 bed2 <- data.frame(seqnames="chr2", start=5, end=8, strand="+")
@@ -15,19 +17,21 @@ empty_cc <- chip_compare$new(empty_grl)
 cc1 <- chip_compare$new(grl1)
 cc2 <- chip_compare$new(grl2)
 
-test_that("Initialize chip_compare - Check param - Valid", {
+### START TESTS
+## initialize
+test_that("chip_compare::initialize - Check param - Valid", {
   expect_that({chip_compare$new(grl1); TRUE}, is_true())
   expect_that({chip_compare$new(grl1, grl2); TRUE}, is_true())
 })
 
-test_that("Initialize chip_compare - Check param - Invalid grl1", {
+test_that("chip_compare::initialize - Check param - Invalid grl1", {
   expect_that(chip_compare$new(), throws_error())
   expect_that(chip_compare$new(1), throws_error())
   expect_that(chip_compare$new("a"), throws_error())
   expect_that(chip_compare$new(list(a=1, b=2)), throws_error())
 })
 
-test_that("Initialize chip_compare - Check param - Invalid grl2", {
+test_that("chip_compare::initialize - Check param - Invalid grl2", {
   expect_that(chip_compare$new(grl1, 1), throws_error())
   expect_that(chip_compare$new(grl1, "a"), throws_error())
   expect_that(chip_compare$new(grl1, list(a=1, b=2)), throws_error())

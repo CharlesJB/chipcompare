d9c93eb42281a5c14a6974382d819bb6323bdb6b
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2015-02-09 08:26:29 -0500

    Corrected error in pvalue function with subject name
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- R/chipcompare.R
+++ R/chipcompare.R
@@ -118,31 +118,32 @@ chipcompare <- R6::R6Class("chipcompare",
       stopifnot(is.numeric(sample_count))
       stopifnot(sample_count > 0)
 
-      # 1. Pre-calculate the overlaps
-      add_overlaps <- function(baseline, subject_name) {
-        # Make sure the seqlevels fit
-        subject <- bai[[subject_name]]
-        levels <- as.character(GenomeInfoDb::seqlevels(subject))
-        levels <- c(levels, as.character(GenomeInfoDb::seqlevels(baseline)))
-        levels <- unique(levels)
-        GenomeInfoDb::seqlevels(baseline) <- levels
-
-        # Calculate the overlaps
-        overlaps <- GenomicRanges::overlapsAny(baseline, subject)
-        GenomicRanges::elementMetadata(baseline)[subject_name] <- overlaps
-        baseline
-      }
-      for (name in names(bai)) {
-        baseline <- add_overlaps(baseline, name)
-      }
-
-      # 2. Produce the matrix
       query <- private$grl[[1]]
       if (length(private$grl) == 2) {
         subject <- private$grl[[2]]
       } else {
         subject <- private$grl[[1]]
       }
+
+      # 1. Pre-calculate the overlaps
+      add_overlaps <- function(baseline, subject_name) {
+        # Make sure the seqlevels fit
+        sbj <- subject[[subject_name]]
+        levels <- as.character(GenomeInfoDb::seqlevels(sbj))
+        levels <- c(levels, as.character(GenomeInfoDb::seqlevels(baseline)))
+        levels <- unique(levels)
+        GenomeInfoDb::seqlevels(baseline) <- levels
+
+        # Calculate the overlaps
+        overlaps <- GenomicRanges::overlapsAny(baseline, sbj)
+        GenomicRanges::elementMetadata(baseline)[subject_name] <- overlaps
+        baseline
+      }
+      for (name in names(subject)) {
+        baseline <- add_overlaps(baseline, name)
+      }
+
+      # 2. Produce the matrix
       launch_pval_calculations <- function(i, sbj_name) {
         j <- which(names(subject) == sbj_name)
         obs <- self$get_matrix()[i,j]
